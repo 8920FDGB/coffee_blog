ad829fcfe3b11f9e014d5ec9d047f461dc9b81d6
8920FDGB <register@old-fshn.com>
2021-10-18 17:57:13 +0900

    Articleモデルのリレーション作成

--- app/Article.php
+++ app/Article.php
@@ -3,8 +3,19 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Article extends Model
 {
-    //
+    // Userモデルへのリレーション
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo('App\User');
+    }
+
+    // Categoryモデルへのリレーション
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo('App\Category');
+    }
 }
