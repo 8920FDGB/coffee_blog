8a7240bc727c91128b3490cbd48de30ac72f6e2e
8920FDGB <register@old-fshn.com>
2021-10-18 20:51:26 +0900

    cssの不要な行を削除

--- resources/views/articles/index.blade.php
+++ resources/views/articles/index.blade.php
@@ -9,7 +9,7 @@
 
   <div class="container">
     <h2 class="pt-4 pb-2 border-bottom">記事一覧</h2>
-    <div class="row py-4 px-2 d-flex flex-nowrap overflow-x">
+    <div class="row py-4 px-2 d-flex flex-nowrap overflow-auto">
       <div class="col-4">
         @include('card')
       </div>
