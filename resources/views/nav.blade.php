25ac1ce0036d5d831f2b8245dc11f6bd0c804ed1
8920FDGB <register@old-fshn.com>
2021-10-19 11:27:12 +0900

    ログアウト機能作成

--- resources/views/nav.blade.php
+++ resources/views/nav.blade.php
@@ -7,17 +7,27 @@
   </button>
   <div class="collapse navbar-collapse" id="navbarSupportedContent-333">
     <ul class="navbar-nav ml-auto">
+      @guest
       <li class="nav-item">
         <a class="nav-link" href="#">ログイン
           <span class="sr-only">(current)</span>
         </a>
       </li>
+      @endguest
+
+      @guest
       <li class="nav-item">
         <a class="nav-link" href="{{ route('register') }}">ユーザー登録</a>
       </li>
+      @endguest
+
+      @auth
       <li class="nav-item">
         <a class="nav-link" href="#">投稿する</a>
       </li>
+      @endauth
+
+      @auth
       <li class="nav-item dropdown">
         <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink-333" data-toggle="dropdown"
           aria-haspopup="true" aria-expanded="false">
@@ -25,11 +35,17 @@
         </a>
         <div class="dropdown-menu dropdown-menu-right dropdown-default"
           aria-labelledby="navbarDropdownMenuLink-333">
-          <a class="dropdown-item" href="#">Action</a>
-          <a class="dropdown-item" href="#">Another action</a>
-          <a class="dropdown-item" href="#">Something else here</a>
+          <a class="dropdown-item" href="#">マイページ</a>
+          <button type="submit" form="logout-button" class="dropdown-item" href="{{ route('logout') }}">
+            ログアウト
+          </button>
         </div>
       </li>
+      <form action="{{ route('logout') }}" method="post" id="logout-button">
+        @csrf
+      </form>
+      @endauth
+
   </div>
 </nav>
 <!--/.Navbar -->
