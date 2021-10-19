2a37f809f3f1d8b8e0af1ebe3ff4a2a7f4347feb
8920FDGB <register@old-fshn.com>
2021-10-19 09:31:00 +0900

    authルーティングの作成と不要なルーティングの削除

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,10 @@
 */
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 
+// 認証系のルーティング（ユーザー登録、ログイン・ログアウト）
+Auth::routes();
+
+// 記事関連のルーティング
 Route::get('/', 'ArticleController@index');
