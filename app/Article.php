39b8cb5a89177e150ff676877436d15527028462
8920FDGB <register@old-fshn.com>
2021-10-25 18:23:24 +0900

    likesを中間とした多対多のリレーション作成とisLikedByメソッド作成

--- app/Article.php
+++ app/Article.php
@@ -4,6 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Article extends Model
 {
@@ -23,5 +24,19 @@ class Article extends Model
     public function category(): BelongsTo
     {
         return $this->belongsTo('App\Category');
+    }
+
+    // likesテーブルを中間テーブルとした、Userモデルとのリレーション（多対多）
+    public function likes(): BelongsToMany
+    {
+        return $this->belongsToMany('App\User', 'likes')->withTimestamps();
+    }
+
+    // あるユーザーがいいね済みかどうかを判定する
+    public function isLikedBy(?User $user)
+    {
+        return $user
+            ? (bool) $this->likes->where('id', $user->id)->count()
+            : false;
     }
 }
