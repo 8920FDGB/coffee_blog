788d8f8a858b6ee08bbc329f9e973692c92e11a6
8920FDGB <register@old-fshn.com>
2021-10-25 18:51:25 +0900

    記事詳細画面にいいねアイコン設置

--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -17,7 +17,7 @@
         <h2 class="font-weight-bold py-3 mb-4">{{ $article->title }}</h2>
 
         {{-- profile --}}
-        <div class="d-flex pb-3 mb-4">
+        <div class="d-flex pb-3 mb-2">
           <!-- Avatar -->
           <img src="https://mdbootstrap.com/img/Photos/Avatars/avatar-8.jpg" class="rounded-circle d-inline-block mr-3" height="30px" width="30px" alt="avatar">
           <!-- Subtitle & username -->
@@ -27,48 +27,53 @@
           </div>
 
           @if (Auth::id() === $article->user_id)
-              <div class="ml-auto">
-                <a href="{{ route('articles.edit', ['article' => $article]) }}" class="btn btn-outline-brown rounded-lg">記事を編集</a>
-                <button type="button" href="" class="btn btn-brown rounded-lg" data-toggle="modal" data-target="#centralModal">
-                  記事を削除
-                </button>
-              </div>
+            <div class="ml-auto">
+              <a href="{{ route('articles.edit', ['article' => $article]) }}" class="btn btn-outline-brown rounded-lg">記事を編集</a>
+              <button type="button" href="" class="btn btn-brown rounded-lg" data-toggle="modal" data-target="#centralModal">
+                記事を削除
+              </button>
+            </div>
 
-              <!-- Central Modal Medium Danger -->
-              <div class="modal fade" id="centralModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel"
-                aria-hidden="true">
-                <div class="modal-dialog modal-notify" role="document">
-                  <!--Content-->
-                  <div class="modal-content">
-                    <!--Header-->
-                    <div class="modal-header brown lighten-2">
-                      <p class="heading lead">ブログ削除</p>
+            <!-- Central Modal Medium Danger -->
+            <div class="modal fade" id="centralModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel"
+              aria-hidden="true">
+              <div class="modal-dialog modal-notify" role="document">
+                <!--Content-->
+                <div class="modal-content">
+                  <!--Header-->
+                  <div class="modal-header brown lighten-2">
+                    <p class="heading lead">ブログ削除</p>
 
-                      <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true" class="white-text">&times;</span>
-                      </button>
-                    </div>
-
-                    <!--Body-->
-                    <form action="{{ route('articles.destroy', ['article' => $article]) }}" method="POST">
-                      @csrf
-                      @method('DELETE')
-                      <div class="modal-body">
-                        <div class="text-center">
-                          <p>ブログを削除します。<br>よろしいですか？</p>
-                        </div>
-                      </div>
-                      <!--Footer-->
-                      <div class="modal-footer justify-content-center">
-                        <a type="button" class="btn btn-outline-brown rounded-lg waves-effect" data-dismiss="modal">キャンセル</a>
-                        <button type="submit" class="btn btn-danger rounded-lg">削除する</button>
-                      </div>
-                    </form>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                      <span aria-hidden="true" class="white-text">&times;</span>
+                    </button>
                   </div>
-                  <!--/.Content-->
+
+                  <!--Body-->
+                  <form action="{{ route('articles.destroy', ['article' => $article]) }}" method="POST">
+                    @csrf
+                    @method('DELETE')
+                    <div class="modal-body">
+                      <div class="text-center">
+                        <p>ブログを削除します。<br>よろしいですか？</p>
+                      </div>
+                    </div>
+                    <!--Footer-->
+                    <div class="modal-footer justify-content-center">
+                      <a type="button" class="btn btn-outline-brown rounded-lg waves-effect" data-dismiss="modal">キャンセル</a>
+                      <button type="submit" class="btn btn-danger rounded-lg">削除する</button>
+                    </div>
+                  </form>
                 </div>
+                <!--/.Content-->
               </div>
+            </div>
           @endif
+        </div>
+
+        <div>
+          <i class="fas fa-heart text-muted p-1 mb-5 mr-1" data-toggle="tooltip" data-placement="top" title="I like it"></i>
+          <span>10</span>
         </div>
 
         {{-- body --}}
