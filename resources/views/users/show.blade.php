f7283e0cd1808d4a164e0994c6d176c18912f1c6
8920FDGB <register@old-fshn.com>
2021-10-26 11:25:56 +0900

    userからarticleへのリレーション作成とマイページで記事一覧表示作成

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -48,12 +48,28 @@
   </ul>
   <div class="tab-content p-3 " id="myClassicTabContent">
     <div class="tab-pane fade active show" id="profile-classic" role="tabpanel" aria-labelledby="profile-tab-classic">
-      <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium,
-        totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta
-        sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia
-        consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui
-        dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora
-        incidunt ut labore et dolore magnam aliquam quaerat voluptatem.</p>
+      <div class="row">
+        @foreach ($user->articles as $article)
+          <div class="col-3 mb-3">
+            @include('card', ['article' => $article])
+          </div>
+        @endforeach
+        <div class="col-3">
+            @include('card', ['article' => $article])
+          </div>
+          <div class="col-3">
+            @include('card', ['article' => $article])
+          </div>
+          <div class="col-3">
+            @include('card', ['article' => $article])
+          </div>
+          <div class="col-3">
+            @include('card', ['article' => $article])
+          </div>
+          <div class="col-3">
+            @include('card', ['article' => $article])
+          </div>
+      </div>
     </div>
     <div class="tab-pane fade" id="follow-classic" role="tabpanel" aria-labelledby="follow-tab-classic">
       <p>Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut
